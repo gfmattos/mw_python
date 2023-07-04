7d05f981aa4cfc3a362201f49b1fb6e4df151420
Guilherme Fernandes de Mattos <gf.mattos21@gmail.com>
2023-07-04 15:03:39 -0400

    Update environment.

--- .venv/Scripts/rst2odt.py
+++ .venv/Scripts/rst2odt.py
@@ -1,6 +1,6 @@
 #!D:\DOCS\my_workspace\lab_house\mw_git\mw_python\.venv\Scripts\python.exe
 
-# $Id: rst2odt.py 8994 2022-01-29 16:28:17Z milde $
+# $Id: rst2odt.py 9115 2022-07-28 17:06:24Z milde $
 # Author: Dave Kuhlman <dkuhlman@rexx.com>
 # Copyright: This module has been placed in the public domain.
 
@@ -11,7 +11,7 @@ A front end to the Docutils Publisher, producing OpenOffice documents.
 try:
     import locale
     locale.setlocale(locale.LC_ALL, '')
-except:
+except Exception:
     pass
 
 from docutils.core import publish_cmdline_to_binary, default_description
